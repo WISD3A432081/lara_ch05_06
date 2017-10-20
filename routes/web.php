1f336c43c540a12ac0aab52a79956497922144ef
chan\admin <s707071127@gmail.com>
2017-10-20 16:09:22 +0800

    將原本執行的閉包參數改為陣列,並將要執行的閉包放入uses中,便能使用as來指定路由的識別名稱

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 })->where(['subject'=>'(chinese| english | math)']);*/
-Route::pattern('student_no','s[0-9]{10}');
+/*Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix'=>'student'],function (){
 Route::get('{student_no}', function ($student_no) {
     return "學號:".$student_no;
@@ -35,4 +35,15 @@ Route::get('{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 })->where(['subject'=>'(chinese| english | math)']);
+});*/
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'],function (){
+    Route::get('{student_no}',['as'=>'student','uses'=>function ($student_no){
+        return '學號:'.$student_no;
+    }
+    ]);
+    Route::get('student/{student_no}/score/{subject?}',['as'=>'student.score','use'=> function ($student_no,$subject=null) {
+        return "學號:" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+    }
+    ])->where(['subject'=>'(chinese| english | math)']);
 });

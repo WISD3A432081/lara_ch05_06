461f1a04c2f74be7dcdf3c290ca03fdb30c3225b
chan\admin <s707071127@gmail.com>
2017-10-20 15:57:12 +0800

    將學號統一進行格式設定，以方便維護路由

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,16 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });*/
-Route::get('student/{student_no}', function ($student_no) {
+/*Route::get('student/{student_no}', function ($student_no) {
     return "學號:" . $student_no;
 })->where(['student_no'=>'s[0-9]{10}']);
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese| english | math)']);
+})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese| english | math)']);*/
+Route::pattern('student_no','s[0-9]{10}');
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
+});
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
+    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+})->where(['subject'=>'(chinese| english | math)']);

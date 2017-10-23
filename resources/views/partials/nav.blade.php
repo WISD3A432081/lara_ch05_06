5dd3fa80cd89c317e42603f30528810938e2d3da
3A432081\Admin <s707071127@gmail.com>
2017-10-23 11:15:30 +0800

    建立一個partials目錄放置局部視圖分別存放head、nav  3A432081陳紹田

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{action('HomeController@index')}}">HelloLaravel</a>
+        <u1 class="nav navbar-nav">
+            <li>
+                <a href="{{action('BoardController@getIndex')}}">排行榜</a>
+            </li>
+        </u1>
+    </div>
+</nav>
+<div style="padding-top:70px;"></div>

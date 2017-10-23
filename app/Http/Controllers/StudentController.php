47b2f33979f29d9c1999ad96ead3754213a5697c
3A432081\Admin <s707071127@gmail.com>
2017-10-23 11:53:06 +0800

    修改控制器內容，將所收到的科目與學號傳給視圖  3A432081陳紹田

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,16 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
-
-class StudentController extends Model
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Student;
+use View;
+class StudentController extends Controller
 {
     public function getStudentData($student_no){
-        return '學號:'.$student_no;
+        return View::make('score',['student_no'=>$student_no,'subject'=>null]);
     }
 
     public function getStudentScore($student_no,$subject=null) {
-        return "學號:" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+        return View::make('score',['student_no'=>$student_no,'subject'=>$subject]);
     }
 }

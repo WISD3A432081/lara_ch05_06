d051cb7c58c8b55bf4a0d6b201a14f17ab8d644e
3A432081陳紹田\admin <s707071127@gmail.com>
2017-11-24 17:15:51 +0800

    修改nav超連結 已可以回到排行榜

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="{{action('HomeController@index')}}">HelloLaravel</a>
         <u1 class="nav navbar-nav">
             <li>
-                <a href="board.blade.php">排行榜</a>
+                <a href="/board">排行榜</a>
             </li>
         </u1>
     </div>
